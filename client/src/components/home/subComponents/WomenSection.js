cb6e70397a2446a593229b88e84d16a2187448f3
xander-here <xander21here@gmail.com>
2022-05-26 19:36:31 +0530

    added avertisement texts

--- client/src/components/home/subComponents/WomenSection.js
+++ client/src/components/home/subComponents/WomenSection.js
@@ -7,7 +7,7 @@ function WomenSection() {
   return (
     <div className=" home__section home__section--women">
       <div className="home__section__content home__section--women__content">
-        <h2>Shop Dresses, Jeans, Dress Pants, Tops & More</h2>
+        <h2>Shop Dresses, Jeans, Tops & More</h2>
         <p>
           Find women's clothing at StandOut. From dresses and jumpsuits, to
           jeans and dress pants, update your wardrobe all in one place. If you
